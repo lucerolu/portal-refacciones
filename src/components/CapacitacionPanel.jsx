37ab7466ad8e83113e949aad4d6cb6fae3cb930e
lucerolu <luceroluna582@gmail.com>
2025-12-09 17:32:19 -0600

    a

--- src/components/CapacitacionPanel.jsx
+++ src/components/CapacitacionPanel.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import MultiLevelDrawer from "./MultiLevelDrawer";
 import MultiLevelMenu from "./MultiLevelMenu";
 import GenericPanel from "./GenericPanel";
+import IframeWithLoader from "./common/IframeWithLoader";
 
 export default function CapacitacionPanel({ isOpen, onClose }) {
   const [activeSubmenu, setActiveSubmenu] = useState(null);
@@ -117,7 +118,7 @@ export default function CapacitacionPanel({ isOpen, onClose }) {
                     {selectedNode.type === "folder" ? (
                       <p className="text-gray-500">Carpeta seleccionada.</p>
                     ) : (
-                      <iframe
+                      <IframeWithLoader
                         src={selectedNode.url}
                         className="flex-1 w-full border rounded-md"
                         allow="fullscreen"
