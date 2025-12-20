d7ab690ec7f43a91f29157ed15e24f96528af697
lucerolu <luceroluna582@gmail.com>
2025-12-20 15:54:45 -0600

    a

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import "./App.css";
 import Navbar from "./components/Navbar";
 import PortalInicio from "./components/PortalInicio";
 import Login from "./components/Login";
