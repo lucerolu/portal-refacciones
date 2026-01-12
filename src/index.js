177aaa8e987d4f7fa8aee1a15096e44ed227d399
lucerolu <luceroluna582@gmail.com>
2026-01-12 17:52:17 -0600

    +app.css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
+import "./app.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
