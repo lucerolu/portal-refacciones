5965bac5006ddfea338c398bd55371349391023a
lucerolu <luceroluna582@gmail.com>
2026-01-13 08:32:39 -0600

    pues no me agarro el app.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import "./app.css";
+import "./App.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -15,7 +15,7 @@ root.render(
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pass a function src\App.css src\index.css
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
